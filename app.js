f143d66ddf3ba9e7315d75c4dfb293177a69e14d
Gyaneshwari sharma <72596558+gswebtutorial@users.noreply.github.com>
2021-08-15 20:58:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+function showclock() {
+    let d = new Date();
+    var h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+
+    if (s > 60) {
+        s = 0;
+        m = m + 1;
+    }
+    if (m > 60) {
+        m = 0;
+        h = h + 1;
+    }
+    if (s < 10) {
+        s = "0" + s;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if(h==0)
+    {
+        h=12;
+    }
+    if(h>12){
+        h=h-12;
+    }
+
+     let clock = document.getElementById("clock");
+    clock.innerHTML = h + ":" + m + ":" + s;
+    setTimeout(showclock, 1000);
+}
